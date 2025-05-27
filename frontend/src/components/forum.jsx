c4db6b62730791a35bab2d929d6e77a251e0018a
Okailey <quaye22d@mtholyoke.edu>
2025-05-27 11:44:31 -0400

    created forum css and jsx

---
+++ frontend/src/components/forum.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import { updateEvent, deleteEvent } from '../utils/CRUDevents';
+import '../styles/forum.css';
+
+
+export default function forum ({}){
+    
+}
+
+
+
