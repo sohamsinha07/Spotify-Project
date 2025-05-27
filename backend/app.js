6f3eb97cb644fad8d966c5ffbead4f854b6a4c6f
Soham Sinha <gha4my@virginia.edu>
2025-05-27 10:58:25 -0400

    Got HTTPS login working for spotify

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,21 @@
 // Import the express module
 const express=require('express');
+const https=require('https');
+const fs=require('fs');
 // Create an instance of the express application
 const app=express();
 // Specify a port number for the server
 const port=5050;
 // Start the server and listen to the port
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const options = {
+  key: fs.readFileSync("test-spotify-site.local-key.pem"),
+  cert: fs.readFileSync("test-spotify-site.local.pem"),
+};
+
+https.createServer(options, app).listen(port, () => {
+  console.log(
+    `HTTPS Server is running on https://test-spotify-site.local:${port}`
+  );
 });
 // use middleware to parse json request bodies
 app.use(express.json());
